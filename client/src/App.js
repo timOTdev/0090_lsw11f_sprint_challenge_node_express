f019e92650948503c04553ff3418d5a92a896bc9
Timothy Hoang <timh1203@gmail.com>
2018-10-05 14:08:25 -0400

    Add cra

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+// import styled, { createGlobalStyle } from 'styled-components'
+// import axios from 'axios'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>App Class Component</h1>
+      </div>
+    )
+  }
+}
+
+// const GlobalStyle = createGlobalStyle`
+//   * {
+//     box-sizing: border-box;
+//   }
+//   html,
+//   body {
+//     width: 100%;
+//     max-width: 1024px;
+//     margin: 0 auto;
+//     padding: 0;
+//     font-size: 62.5%;
+//     border: 1px solid black;
+//     text-align: center;
+//   }
+//   h1 {
+//     font-size: 3rem;
+//   }
+//   h2 {
+//     font-size: 2.4rem;
+//   }
+//   h3 {
+//     font-size: 2rem;
+//   }
+//   p {
+//     font-size: 1.6rem;
+//   }
+// `
+// const Div1 = styled.div``
+// const H11 = styled.h1``
+
+export default App
