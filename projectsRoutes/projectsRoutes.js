ab93d96a0b7ca29442aa64fa934af0f9344d1701
Timothy Hoang <timh1203@gmail.com>
2018-10-05 12:11:48 -0400

    Add GET for projects route with all project actions

--- projectsRoutes/projectsRoutes.js
+++ projectsRoutes/projectsRoutes.js
@@ -30,4 +30,15 @@ router.route('/:id')
       .catch(err => res.status(500).json({ error: "The project information could not be retrieved." }));
   })
 
+  router.route('/:id/actions')
+    .get((req, res) => {
+      const { id } = req.params;
+      projectsDb.getProjectActions(id)
+        .then(allActions => {
+          if (!allActions.length) return res.status(404).json({ error: "The actions of the project with the specified ID does not exist."});
+          return res.status(200).json(allActions);
+        })
+        .catch(err => res.status(500).json({ error: "The actions of the projects could not be retrieved" }));
+    })
+
 module.exports = router;
