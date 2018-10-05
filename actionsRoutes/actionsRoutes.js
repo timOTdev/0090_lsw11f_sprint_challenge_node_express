53118da85dcea80c4bb22592f5400d65b8800e85
Timothy Hoang <timh1203@gmail.com>
2018-10-05 13:22:03 -0400

    Add POST for actions route

--- actionsRoutes/actionsRoutes.js
+++ actionsRoutes/actionsRoutes.js
@@ -1,9 +1,25 @@
 // POSTS ROUTE HANDLERS
 const express = require('express');
 const router = express.Router();
+const projectsDb = require('../data/helpers/projectModel.js');
 const actionsDb = require('../data/helpers/actionModel.js');
 
 router.route('/')
+  .post((req, res) => {
+    const { project_id, description, notes, completed } = req.body;
+    const createdAction = { project_id, description, notes, completed: completed || false };
+    if (!project_id) return res.status(400).json({ error: "Please provide a project id." });
+    if (!description) return res.status(400).json({ error: "Please provide a description." });
+    if (description.length > 128) return res.status(422).json({ error: `Please provide a description less than or equal to 128 characters long. (${description.length})` });
+    if (!notes) return res.status(400).json({ error: "Please provide notes." });
+    projectsDb.get(project_id)
+      .then(project => {
+        actionsDb.insert(createdAction)
+          .then(newAction => res.status(201).json(newAction))
+          .catch(err => res.status(500).json({ error: "There was an error while saving the action to the database." }));
+      })
+      .catch(err => res.status(500).json({ error: "The project with the specified ID does not exist." }));
+  })
   .get((req, res) => {
     actionsDb.get()
       .then(actions => res.status(200).json(actions))
@@ -11,4 +27,3 @@ router.route('/')
   })
 
   module.exports = router;
-  
