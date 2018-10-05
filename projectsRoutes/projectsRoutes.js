378e23b0edc540bf0142bdf5985c9606ff2bc34a
Timothy Hoang <timh1203@gmail.com>
2018-10-05 12:03:09 -0400

    Add GET for projects route with specific project

--- projectsRoutes/projectsRoutes.js
+++ projectsRoutes/projectsRoutes.js
@@ -19,4 +19,15 @@ router.route('/')
       .catch(err => res.status(500).json({ error: "The request for projects could not be retrieved." }));
   })
 
+router.route('/:id')
+  .get((req, res) => {
+    const { id } = req.params;
+    projectsDb.get(id)
+      .then(project => {
+        if (!project) return res.status(404).json({ error: "The project with the specified ID does not exist." });
+        return res.status(200).json(project);
+      })
+      .catch(err => res.status(500).json({ error: "The project information could not be retrieved." }));
+  })
+
 module.exports = router;
